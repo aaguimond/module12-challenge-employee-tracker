cb84c21a76e578c7781800c6238ff1a6f1ffe002
aaguimond <160686688+aaguimond@users.noreply.github.com>
2024-05-19 00:24:40 -0500

    possibly fixed logic. Will test more tomorrow

--- sql/tables.sql
+++ sql/tables.sql
@@ -7,13 +7,13 @@ CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     salary INTEGER NOT NULL,
-    department_id INTEGER FOREIGN KEY references department(id)
+    department_id INTEGER REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    role_id INTEGER FOREIGN KEY references role(id),
-    manager_id INTEGER references employee(id)
+    role_id INTEGER REFERENCES role(id),
+    manager_id INTEGER REFERENCES employee(id)
 );
